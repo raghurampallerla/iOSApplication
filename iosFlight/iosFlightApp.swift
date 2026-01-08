cd9c7b579735b89b59e76dbe30c4ff31802919ed
raghurampallerla <raghuram.pallerla@gmail.com>
2026-01-08 18:52:31 +0000

    Initial Commit

---
+++ iosFlight/iosFlightApp.swift
@@ -0,0 +1,32 @@
+//
+//  iosFlightApp.swift
+//  iosFlight
+//
+//  Created by RAGHURAM PALLERLA on 08/01/2026.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct iosFlightApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ iosFlightTests/iosFlightTests.swift
@@ -0,0 +1,16 @@
+//
+//  iosFlightTests.swift
+//  iosFlightTests
+//
+//  Created by RAGHURAM PALLERLA on 08/01/2026.
+//
+
+import Testing
+
+struct iosFlightTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
